89a9ee893c37ea2f2d250011f0fe75c4dfa490c7
Leon Federico Huck <leonhuck007@gmail.com>
2025-10-27 22:13:20 -0300

    feat: remove default values in database configuration to enforce environment variable usage

--- src/config/config.js
+++ src/config/config.js
@@ -2,25 +2,25 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "L4i0n_f4k3007",
-    database: process.env.DB_NAME || "restaurant_app",
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   test: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || "database_test",
-    host: process.env.DB_HOST || "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || "database_production",
-    host: process.env.DB_HOST || "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
     dialectOptions: {
       ssl: {
--- src/createDatabase.js
+++ src/createDatabase.js
@@ -4,9 +4,9 @@ const createDataBase = async () =>
 {
     try{
         const connection = await mysql.createConnection({
-            host: process.env.DB_HOST || 'localhost',
-            user: process.env.DB_USER || 'root',
-            password: process.env.DB_PASSWORD || 'L4i0n_f4k3007',
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
             });
         await connection.query('CREATE DATABASE IF NOT EXISTS restaurant_app;')
         console.log('La base de datos se creo o ya existia')
